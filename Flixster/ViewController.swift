989e70534d9bd5356f33e08e72fe93568c0481e1
Jonathan Velez <110303080+Jav0918@users.noreply.github.com>
2023-03-06 22:36:26 -0600

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Jonathan Velez on 3/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
